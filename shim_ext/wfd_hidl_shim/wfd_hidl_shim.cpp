cc02c68d546fb8b07d84c6abfb75e898a1396d74
Dmitry <42121541+dkpost3@users.noreply.github.com>
2024-12-06 09:04:47 +0300

    Add files via upload

---
+++ shim_ext/wfd_hidl_shim/wfd_hidl_shim.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+namespace android {
+    extern "C" void _ZN7android11MotionEvent10initializeEiijiNSt3__15arrayIhLm32EEEiiiiiiNS_20MotionClassificationERKNS_2ui9TransformEffffS8_llmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE() {}
+    extern "C" void _ZN7android17PointerProperties8copyFromERKS0_() {}
+    extern "C" void _ZN7android30android_view_KeyEvent_toNativeEP7_JNIEnvP8_jobject() {}
+    extern "C" void _ZN7android32android_view_KeyEvent_fromNativeEP7_JNIEnvRKNS_8KeyEventE() {}
+    extern "C" void _ZN7android8KeyEvent10initializeEiijiNSt3__15arrayIhLm32EEEiiiiiill() {}
+    extern "C" void _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf() {}
+    extern "C" void _ZN7android21SurfaceComposerClient14destroyDisplayERKNS_2spINS_7IBinderEEE() {}
+}
